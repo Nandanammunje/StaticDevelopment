62510ade1b14bcd92da39c22e558a9c35eafed4b
Nandanammunje <nandannayak15@gmail.com>
2021-04-18 23:23:12 +0530

    add backend contstant

--- FileCopyingSystem/src/com/nuance/constants/BackendConstants.java
+++ FileCopyingSystem/src/com/nuance/constants/BackendConstants.java
@@ -6,4 +6,5 @@ public class BackendConstants {
 	
 	public static final String EXTEXCLUDED=".pdf";
 	public static final String FILESOURCE="C:\\logs\\Replica\\";
+	public static final String PATHSEPERATOR="\\";
 }
